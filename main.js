1c4650c63004bac79cb39597bcc2a1977a28e90d
unknown <gowthi56@gmail.com>
2023-03-01 12:43:50 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+let inputFood = document.getElementById("input-food");
+let inputBtn = document.getElementById("input-btn");
+let foodContainer = document.getElementById("food-container");
+let noListEl = document.getElementById("no-list"); //image
+const foodListStatistics = document.getElementById("food-list-statistics"); //number
+
+const handleInputFood = () => {
+  // creating li element
+  let newFoodItemEl = document.createElement("li");
+
+  let div = document.createElement("div"); //for list-item
+  let divBtn = document.createElement("div");
+
+  newFoodItemEl.append(div, divBtn);
+
+  // assigning textContent & className to newFoodItemEl
+  div.textContent = inputFood.value;
+  newFoodItemEl.className = "food-item";
+
+  divBtn.parentElement.setAttribute("onClick", "removeFoodItem(event)");
+  divBtn.innerHTML = `<i class="fa fa-xmark"></i>`;
+  newFoodItemEl.append(divBtn);
+
+  // appending newly created element(newFoodItemEl) to foodContainer
+  foodContainer.append(newFoodItemEl);
+
+  // resetting the inputFood value
+  inputFood.value = "";
+
+  refreshUI();
+};
+
+inputBtn.addEventListener("click", handleInputFood);
+
+inputFood.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    handleInputFood();
+  } else if (event.key === "KeyZ" && (event.ctrlKey || event.metaKey)) {
+    // Undo Operations
+    inputFood.value = "";
+  }
+});
+
+// remove Food items
+function removeFoodItem(event) {
+  const existingList = event.target.parentNode.parentNode;
+  existingList.remove();
+
+  refreshUI();
+}
+
+function refreshUI() {
+  foodListStatistics.innerText = `You have ${foodContainer.children.length} lists`;
+
+  if (foodContainer.children.length > 0) {
+    //  children exist, so don't show `no-list` div
+    noListEl.hidden = true;
+  } else {
+    // children not exist, show `no-list` div
+    noListEl.hidden = false;
+  }
+
+  // You can write better!
+//   foodContainer.children.length > 0
+//     ? ((noListEl.hidden = true), (foodListStatistics.hidden = false))
+//     : ((noListEl.hidden = false), (foodListStatistics.hidden = true));
+}
